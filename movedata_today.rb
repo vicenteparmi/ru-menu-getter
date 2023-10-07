da7c9dda69e58dece1b1bd832d89b990f4fbd261
Vicente Parmigiani <vicenteparmigiani@gmail.com>
2023-10-07 19:39:06 -0300

    Trying to add a copy of the data to firestore

--- movedata_today.rb
+++ movedata_today.rb
@@ -2,12 +2,14 @@
 # Author: @vicenteparmi
 
 require 'firebase'
+require "google/cloud/firestore"
 
 def moveData()
     # Initialize Firebase
     base_uri = ENV['BASE_URL']
     secret = ENV['FIREBASE_KEY']
     firebase = Firebase::Client.new(base_uri, secret)
+    firestore = Google::Cloud::Firestore.new(base_uri, secret)
   
     # Get the references to the old and new data
     old_ref = firebase.get("menus")
@@ -18,10 +20,16 @@ def moveData()
       city_data["rus"].each do |ru, ru_data|
         begin
           ru_data["menus"].each do |date, date_data|
-            # Copy the data to the archive
+            # Copy the data to the archive on Firebase Realtime Database
             response = firebase.get("menus/#{city}/rus/#{ru}/menus/#{date}")
-            response = firebase.set("archive/menus/#{city}/rus/#{ru}/menus/#{date}", response.body)
-  
+            firebase.set("archive/menus/#{city}/rus/#{ru}/menus/#{date}", response.body)
+
+            # Copy the data to the archive on Firebase Firestore, divided in 'coffee', 'lunch' and 'dinner'
+            archive = firestore.collection("archive").doc("menus").collection(city).doc("rus").collection(ru).doc("menus").collection(date)
+            archive.doc("coffee").set(response.body[0])
+            archive.doc("lunch").set(response.body[1])
+            archive.doc("dinner").set(response.body[2])
+
             # Return the response
             puts "[ADDING FOR TODAY] Finished #{ru} for #{date}."
   
